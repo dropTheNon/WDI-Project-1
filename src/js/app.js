8702860e8a765b20e1b886566a8b69960297dfb8
Josh McBroom <mcbroomjc@gmail.com>
2016-06-04 23:53:29 -0700

    Combined compareResults function into Action function

--- src/js/app.js
+++ src/js/app.js
@@ -42,8 +42,6 @@ var diceRoll = function() {
   return Math.floor(Math.random() * 6) + 1;
 };
 
-// Pushing dice roll results into arrays
-
 // Function to sort and reverse array of dice results
 var sortArray = function(array) {
   return array.sort().reverse();
@@ -56,47 +54,59 @@ if (effectiveTroops > 0) {
   // add to dropdown menu as valid attack-from option
 };
 
-// Attackers Dice Count
-if (effectiveTroops < 2) {
-  attackerDiceCount = 1;
-} else if (effectiveTroops < 3) {
-  attackerDiceCount = 2;
-} else {
-  attackerDiceCount = 3;
-};
-// Defenders Dice Count
-if (defTerritory.troops > 1) {
-  defenderDiceCount = 2;
-} else {
-  defenderDiceCount = 1;
+
+/* ================ Deploy Troops Function ================ */
+// This function lets you deploy your new troops (usually 3) each round,
+// before starting your attacks.
+
+var deploy = function(troopsToDeploy, territoryToDeployOnto) {
+  // Create dropdown menu option for troopsToDeploy
 }
-// Rolling Dice for each player
-for (i = attackerDiceCount; i > 0; i--) {
-  attackerDiceResults.push(diceRoll());
-};
-for (i = defenderDiceCount; i > 0; i--) {
-  defenderDiceResults.push(diceRoll());
-};
 
 
-/* ================ CompareResults Function =============== */
+/* ================ Main Function - Attack =============== */
+// This function lets you input the attacking & defending territories,
+// defined as indices in array territories, and changes values
+// for territories' troops based on how the dice fell. There is a
+// check for win conditions (attacker successfully takes a territory),
+// and then will allow the attacker to advance troops.
 
 
-// Comparing dice results of two sorted, reversed arrays (of possibly different lengths)
-// Plugging in the to/from territories makes it easier to change troop #s w/o having to find index in our while loop.
-// For attTerritory and defTerritory we can pass those in as attTerritories[x] and defTerritories[x]
-var compareResults = function(attackerDiceResults, defenderDiceResults, attTerritory, defTerritory) {
+var attack = function(attTerritory, defTerritory) {
+  var effectiveTroops = attTerritory.troops - 1;
+  // Attackers Dice Count
+  if (effectiveTroops < 2) {
+    attackerDiceCount = 1;
+  } else if (effectiveTroops < 3) {
+    attackerDiceCount = 2;
+  } else {
+    attackerDiceCount = 3;
+  }
+  // Defenders Dice Count
+  if (defTerritory.troops > 1) {
+    defenderDiceCount = 2;
+  } else {
+    defenderDiceCount = 1;
+  }
+  // Rolling Dice for each player, pushing into their Array
+  for (var i = attackerDiceCount; i > 0; i--) {
+    attackerDiceResults.push(diceRoll());
+  }
+  for (var j = defenderDiceCount; j > 0; j--) {
+    defenderDiceResults.push(diceRoll());
+  }
+
+  // Comparing dice results of two sorted, reversed arrays (of possibly different lengths)
   var sortedAttackerArray = sortArray(attackerDiceResults);
   var sortedDefenderArray = sortArray(defenderDiceResults);
-  var i = 0;
-  while(i < sortedAttackerArray.length && i < sortedDefenderArray.length) {
-    if (sortedAttackerArray[i] > sortedDefenderArray[i]) {
+  var k = 0;
+  while(k < sortedAttackerArray.length && k < sortedDefenderArray.length) {
+    if (sortedAttackerArray[k] > sortedDefenderArray[k]) {
       defTerritory.troops --;
     } else {
       attTerritory.troops --;
     }
-    i++;
-
+    k++;
     // Conquering a territory
     if (defTerritory.troops === 0) {
       attTerritory.troops --;
@@ -107,5 +117,7 @@ var compareResults = function(attackerDiceResults, defenderDiceResults, attTerri
       /* Create drop-down menu function with choices for (attTerritory.troops - 1) */
       return alert(/* Good-looking pop-up to choose how many troops to advance */)
     };
-  }
-}
+  };
+  // jQuery to update the values of changed territories, refocus on attackBox,
+  // display message in/near attackBox (alert?) stating result.
+};
